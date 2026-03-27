06eadea72814b09c9be931437529b0d2fbb4cefb
prince <prince@localhost.localdomain>
2026-03-27 15:00:02 +0530

    comment removed

--- setup.sh
+++ setup.sh
@@ -44,10 +44,10 @@ userPass=$(gum input --placeholder "Enter a password: ")
 userPass_hash=$(echo $userPass | sha256sum)
 echo "${userPass_hash}" > .T_log/userpass
 
-*cd 
-*cd ..
-*cd usr/etc
-*echo 'exec ./T_log.sh' >> bash.bashrc
-*echo ""
-*echo -e "🔐 \e[0;1mUsername and password set sucessfully."
+cd 
+cd ..
+cd usr/etc
+echo 'exec ./T_log.sh' >> bash.bashrc
+echo ""
+echo -e "🔐 \e[0;1mUsername and password set sucessfully."
 
